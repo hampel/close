969a517ee242504ceea3833d19c21dfb79fe981a
Simon Hampel <simon@hampelgroup.com>
2015-11-30 11:12:50 +1100

    renamed getLeads to queryLeads; added getCustomFields function; added addNote function

--- src/CloseIo.php
+++ src/CloseIo.php
@@ -12,12 +12,29 @@ class CloseIo
 		$this->client = $client;
 	}
 
+	/**
+	 * Fetch details about a single lead
+	 *
+	 * @param $id	lead ID
+	 *
+	 * @return array
+	 */
 	public function getLead($id)
 	{
 		return $this->client->get("lead/{$id}/");
 	}
 
-	public function getLeads($query = null, $_limit = null, $_skip = null, array $fields = [])
+	/**
+	 * Search for leads based on query criteria
+	 *
+	 * @param null $query	optional query (if not specified, all leads will be returned)
+	 * @param null $_limit	limit the number of leads to be returned (default: 100)
+	 * @param null $_skip	skip leads for paging
+	 * @param array $fields	which fields to be returned in the results - helps with performance
+	 *
+	 * @return array
+	 */
+	public function queryLeads($query = null, $_limit = null, $_skip = null, array $fields = [])
 	{
 		$_fields = empty($fields) ? null : implode(',', $fields);
 
@@ -26,8 +43,38 @@ class CloseIo
 		return $this->client->get("lead/" . (empty($querystring) ? "" : "?{$querystring}"));
 	}
 
+	/**
+	 * Create a new lead
+	 *
+	 * @param Lead $lead the lead to be created
+	 *
+	 * @return array
+	 */
 	public function createLead(Lead $lead)
 	{
 		return $this->client->post("lead/", $lead->toArray());
+	}
+
+	/**
+	 * Retrieve details of all custom fields defined in the system
+	 *
+	 * @return array
+	 */
+	public function getCustomFields()
+	{
+		return $this->client->get("custom_fields/lead/");
+	}
+
+	/**
+	 * Add a note to a lead
+	 *
+	 * @param $note		string - content for the note (no HTML)
+	 * @param $lead_id	the ID of the lead to add the note to
+	 *
+	 * @return array
+	 */
+	public function addNote($note, $lead_id)
+	{
+		return $this->client->post("activity/note/", compact('note', 'lead_id'));
 	}
 }
