2b82272f70d78ecfa316971f80d3cfa005c1dce3
hampel <simon@hampelgroup.com>
2021-09-25 16:27:47 +1000

    updated EmailAddress - empty email is not permitted; new docs; new static function: fullEmail used for array_map

--- src/Types/EmailAddress.php
+++ src/Types/EmailAddress.php
@@ -4,12 +4,25 @@ use Close\Exception\InvalidArgumentException;
 
 class EmailAddress
 {
+    /** @var string  */
 	private $email;
 
+	/** @var string  */
 	private $name;
 
+    /**
+     * EmailAddress constructor.
+     *
+     * @param string $email
+     * @param string $name
+     */
 	function __construct($email, $name = '')
 	{
+        if (empty($email))
+        {
+            throw new InvalidArgumentException("email is required");
+        }
+
 		if (preg_match('/^(?:(.*?)\s<?)?([A-Z0-9._%+-]+@[A-Z0-9.-]+)>?$/im', $email, $matches)) {
 			$name = $matches[1] ? $matches[1] : $name;
 			$email = $matches[2];
@@ -27,24 +40,36 @@ class EmailAddress
 		$this->name = $name;
 	}
 
+    /**
+     * @param string $email
+     * @param string $name
+     *
+     * @return EmailAddress
+     */
 	public static function createEmailAddress($email, $name = '')
 	{
 		return new self($email, $name);
 	}
 
 	/**
-	 * @return mixed
+	 * @return string
 	 */
 	public function getEmail()
 	{
 		return $this->email;
 	}
 
+    /**
+     * @return string
+     */
 	public function getName()
 	{
 		return $this->name;
 	}
 
+    /**
+     * @return string
+     */
 	public function getFull()
 	{
 		$name = $this->getName();
@@ -52,4 +77,14 @@ class EmailAddress
 
 		return empty($name) ? $email : "{$name} <{$email}>";
 	}
+
+    /**
+     * @param EmailAddress $email
+     *
+     * @return string
+     */
+    public static function fullEmail(EmailAddress $email)
+    {
+        return $email->getFull();
+    }
 }
