b9f27e1095fd95d77cc03c014e6b24814a586ca1
hampel <simon@hampelgroup.com>
2021-09-25 14:32:03 +1000

    change Note activity to expect only required parameters

--- src/Types/Activity/Note.php
+++ src/Types/Activity/Note.php
@@ -1,34 +1,76 @@
 <?php namespace Close\Types\Activity;
 
 use Close\Arrayable;
+use Close\Exception\InvalidArgumentException;
 
 class Note implements Arrayable
 {
-	private $note;
+    /** @var string */
+    private $lead_id;
 
-	private $lead_id;
+    /** @var string */
+    private $note;
 
-	function __construct($note, $lead_id)
-	{
-		$this->note = $note;
-		$this->lead_id = $lead_id;
-	}
+    /**
+     * Note constructor.
+     *
+     * @param string $lead_id
+     * @param string $note
+     *
+     * @throws InvalidArgumentException
+     */
+    public function __construct($lead_id, $note)
+    {
+        if (empty($lead_id))
+        {
+            throw new InvalidArgumentException("lead_id is required");
+        }
 
-	/**
-	 * @return mixed
-	 */
-	public function getNote()
-	{
-		return $this->note;
-	}
+        if (empty($note))
+        {
+            throw new InvalidArgumentException("note is required");
+        }
 
-	/**
-	 * @return mixed
-	 */
-	public function getLeadId()
-	{
-		return $this->lead_id;
-	}
+        $this->setLeadId($lead_id);
+        $this->setNote($note);
+    }
+
+    /**
+     * @return string
+     */
+    public function getLeadId()
+    {
+        return $this->lead_id;
+    }
+
+    /**
+     * @param string $lead_id
+     */
+    public function setLeadId($lead_id)
+    {
+        if (!empty($lead_id) AND substr($lead_id, 0, 5) !== 'lead_')
+        {
+            throw new InvalidArgumentException("Invalid lead_id [{$lead_id}]");
+        }
+
+        $this->lead_id = $lead_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    /**
+     * @param string $note
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+    }
 
 	/**
 	 * @return array
@@ -36,8 +78,8 @@ class Note implements Arrayable
 	public function toArray()
 	{
 		return [
-			'note' => $this->getNote(),
-			'lead_id' => $this->getLeadId(),
+            'lead_id' => $this->getLeadId(),
+            'note' => $this->getNote(),
 		];
 	}
 }
---
+++ tests/Types/Activity/NoteTest.php
@@ -0,0 +1,49 @@
+<?php namespace Close\Types\Activity;
+
+use Close\Exception\InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class NoteTest extends TestCase
+{
+    public function test_Note_no_lead_throws_exception()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('lead_id is required');
+
+        new Note(null, null);
+    }
+
+    public function test_Note_no_note_throws_exception()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('note is required');
+
+        new Note('foo', null);
+    }
+
+    public function test_Note_with_data()
+    {
+        $note = new Note('lead_foo', 'note foo');
+
+        $this->assertEquals('lead_foo', $note->getLeadId());
+        $this->assertEquals('note foo', $note->getNote());
+
+        $data = $note->toArray();
+
+        $this->assertEquals(['lead_id' => 'lead_foo', 'note' => 'note foo'], $data);
+    }
+
+    public function test_Note_set_new_data()
+    {
+        $note = new Note('lead_foo', 'note foo');
+        $note->setLeadId('lead_foo2');
+        $note->setNote('note foo2');
+
+        $this->assertEquals('lead_foo2', $note->getLeadId());
+        $this->assertEquals('note foo2', $note->getNote());
+
+        $data = $note->toArray();
+
+        $this->assertEquals(['lead_id' => 'lead_foo2', 'note' => 'note foo2'], $data);
+    }
+}
