90ff22e37611aafc262cb147c07ce65d535cc6bb
hampel <simon@hampelgroup.com>
2021-09-27 11:51:02 +1000

    Close test

--- tests/CloseTest.php
+++ tests/CloseTest.php
@@ -3,6 +3,7 @@
 use Close\Close;
 use Close\CloseClient;
 use Close\Exception\InvalidArgumentException;
+use Close\Types\Lead\Lead;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
@@ -21,11 +22,20 @@ class CloseTest extends TestCase
         $this->close = new Close($this->client);
     }
 
-    public function test_getLead()
+    public function test_getLead_no_fields()
     {
         $this->client->expects()->get('lead/foo/')->once()->andReturns([]);
 
         $lead = $this->close->getLead('foo');
+
+        $this->assertIsArray($lead);
+    }
+
+    public function test_getLead_with_fields()
+    {
+        $this->client->expects()->get('lead/foo/?_fields=fieldA%2CfieldB')->once()->andReturns([]);
+
+        $lead = $this->close->getLead('foo', ['fieldA', 'fieldB']);
 
         $this->assertIsArray($lead);
     }
@@ -44,6 +54,24 @@ class CloseTest extends TestCase
         $this->client->expects()->get('lead/?query=company%3A%22foo%22')->once()->andReturns([]);
 
         $lead = $this->close->queryLeads('company:"foo"');
+
+        $this->assertIsArray($lead);
+    }
+
+    public function test_queryLeads_with_params()
+    {
+        $this->client->expects()->get('lead/?query=company%3A%22foo%22&_limit=10&_skip=5&_fields=fieldA%2CfieldB')->once()->andReturns([]);
+
+        $lead = $this->close->queryLeads('company:"foo"', 10, 5, ['fieldA', 'fieldB']);
+
+        $this->assertIsArray($lead);
+    }
+
+    public function test_createLead()
+    {
+        $this->client->expects()->post('lead/', ['name' => 'foo'])->once()->andReturns([]);
+
+        $lead = $this->close->createLead(new Lead('foo'));
 
         $this->assertIsArray($lead);
     }
